c1cc1496ec605b23b7ac972532fd7d50af69bdfa
nusrat-jahan-web <nusratjahanweb89@gmail.com>
2024-11-16 12:10:18 +0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+// This function is to show hello world
+
+
+/*\
+ 1, dfdff
+ 3 dfd
+4. multiline comments
+
+*/
+//dfdfd
+
+
+// biyog
+
+
+function substractionNumber (number1, number2){
+    var  result = number1 / number2;
+
+   return result;
+
+}
+var substractionResult1= substractionNumber(1024, 128);
+var substractionResult2= substractionNumber(40, 20);
+var substractionResult3= substractionNumber(30, 20);
+
+alert(substractionResult1);
+alert(substractionResult2);
+alert(substractionResult3);
+
+function sumNumber (){
+    var number1 = 40;
+    var number2 = 20;
+
+    var result= number1+ number2;
+
+    alert (result);
+
+}
+sumNumber ();
+
+
+
+
+
+
+
+var students =["nusrat"]
